73faecb58571cfa3fc228c689c12c51e83f5db7a
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-07-15 09:59:49 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const { join } = require('path');
+
+const url = "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof";
+
+const scrapeResult = {
+  title: 'EAH IS HIRING FOR A BUSINESS SYSTEMS IMPLEMENTATION ANALYST',
+  description: 'Position is for a full-time Systems Implementation Analyst to work in the EAH Corporate office located in San Rafael.',
+  datePosted: new Date('2020-06-13'),
+  url: 'https://sfbay.craigslist.org/nby/sof/d/san-quentin-eah-is-hiring-for-business/7159011735.html',
+  hood: '(san rafael)',
+  address: '22 Pelican Way',
+  compensation: 'Salary commensurate to experience.'
+}
+
+const scrapeResults = []
+
+async function scrapeJobHeader() {
+  try {
+    const htmlResult = await request.get(url);
+    const $ = await cheerio.load(htmlResult);
+
+    $('.result-info').each((index, element) => { 
+      const resultTitle = $(element).children(".result-title");
+      const resultDate = $(element).children(".result-date");
+      const title = resultTitle.text();
+      const url = resultTitle.attr("href");
+      const datePosted = new Date(resultDate.attr("datetime"));
+      const hood = $(element).find(".result-hood").text();
+      const scrapeResult = { title, url, datePosted, hood };
+      scrapeResults.push(scrapeResult)
+    })
+    return scrapeResults;
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function scrapeDescription(jobsWithHeaders) {
+  return await Promise.all(jobsWithHeaders.map(async (job) => {
+    try {
+      const htmlResult = await request.get(job.url);
+      const $ = await cheerio.load(htmlResult);
+      $(".print-qrcode-container").remove();
+      job.description = $("#postingbody").text();
+      job.address = $("div.mapaddress").text();
+      const compensationText = $(".attrgroup").children().first().text();
+      job.compensation = compensationText.replace("compensation: ", "");
+      return job;
+    } catch(err) {
+      console.log(err);
+    }
+  }))
+}
+
+async function scrapeCraigslist() {
+  const jobsWithHeaders = await scrapeJobHeader();
+  const jobsFullData = await scrapeDescription(jobsWithHeaders);
+  console.log(jobsFullData);
+  console.log(jobsWithHeaders.length);
+}
+
+scrapeCraigslist();
